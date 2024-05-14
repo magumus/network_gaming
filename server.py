64c21d2c724ec0a1cd4db72ec02466c51a03afcc
Anıl <aanilakgun@gmail.com>
2024-05-14 22:21:37 +0300

    "initial dosya"

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import socket
 from _thread import *
+from player import Player
+import pickle
 import sys
 
 server = "192.168.1.104"
@@ -15,21 +17,14 @@ except socket.error as e:
 s.listen(2)
 print("waiting for connection, server started")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
-
-pos = [(0,0),(100,100)]
+players = [Player(0,0,50,50,(255,0,0)),Player(100,100,50,50,(0,255,0))]
 def threaded_client(conn, player):
-    conn.send(str.encode(make_pos(pos[player])))
+    conn.send(pickle.dumps(players[player]))
     reply = ""
     while True:
         try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+            data = pickle.loads(conn.recv(2048))
+            players[player] = data
 
 
             if not data:
@@ -37,13 +32,13 @@ def threaded_client(conn, player):
                 break
             else:
                 if player == 1:
-                    reply = pos[0]
+                    reply = players[0]
                 else:
-                    reply = pos[1]
+                    reply = players[1]
                 print("Received:", data)
                 print("Sending:", reply)
 
-            conn.sendall(str.encode(make_pos(reply)))
+            conn.sendall(pickle.dumps(reply))
         except:
             break
 
